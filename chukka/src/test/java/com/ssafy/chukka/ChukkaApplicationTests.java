0d544d27cc7ec4b1cd1bf55da7ff20f8ffac67a5
KimDaeyeong <daeyoung4088@hanmail.net>
2022-07-20 12:55:49 +0900

    ✨back
    first commit of backend

---
+++ chukka/src/test/java/com/ssafy/chukka/ChukkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.chukka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChukkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
