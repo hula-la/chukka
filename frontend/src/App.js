b2462c827cf89202d5af8a423b5d57810de81de5
홍성목 <swff07183@gmail.com>
2022-07-25 17:53:43 +0900

    ✨ 프론트엔드 프로젝트 생성
    - frontend 폴더 생성
    - 프로젝트 생성 및 package.json 수정

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import { Route, Routes } from 'react-router-dom';
+import IndexPage from './pages/IndexPage';
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<IndexPage />} />
+    </Routes>
+  );
+};
+
+export default App;
