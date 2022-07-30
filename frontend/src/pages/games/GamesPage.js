2985c5623cbc6109028d191cd7e0b7a3bdd6b67d
yeonui <dusdml1502@naver.com>
2022-07-30 10:38:23 +0900

    ✨ : 강사 영상 재생 & 웹소켓 통신 확인
    - 강사 영상 재생 확인
    - 웹 소켓으로 통신 후, 리액트에서 데이터 수신 확인

--- frontend/src/pages/games/GamesPage.js
+++ frontend/src/pages/games/GamesPage.js
@@ -1,10 +1,10 @@
-import SingleMode from "../../components/games/SingleMode";
+import SingleGame from "../../components/games/SingleGame";
 
 const GamesPage = () => {
   return (
     <div>
       <h1>GamesPage</h1>
-      <SingleMode></SingleMode>
+      <SingleGame></SingleGame>
     </div>
   );
 };
