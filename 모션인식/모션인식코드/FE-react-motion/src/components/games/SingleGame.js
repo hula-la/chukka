df82daa6d303a9e0ccd32060ba896184e161c4c6
soojin <ksgf988@naver.com>
2022-08-05 15:25:22 +0900

    ✨:게임이펙트 추가 & FPS 0.5로 변경
    - 유사도에 따른 게임 이펙트 추가
      - setGameEF 쪽 문제 발생, 디버깅 예정
    - 라즈베리파이에서의 작동을 위한 FPS 감소
    
    Former-commit-id: 4cd3f85cd602278bb68e61420d938bdad1305760

--- 모션인식/모션인식코드/FE-react-motion/src/components/games/SingleGame.js
+++ 모션인식/모션인식코드/FE-react-motion/src/components/games/SingleGame.js
@@ -5,6 +5,22 @@ import axios from 'axios';
 
 const SingleMode = () => {
 
+  const [websckt, setWebsckt] = useState();  
+  const [FPS, setFPS] = useState(5);  
+  const [Iv, setIv] = useState(null);  
+  const [playing, setPlaying] = useState(undefined);
+  const [timer, setTimer] = useState(undefined)
+  const [songID, setSongID] = useState(null);
+
+  // 게임 이펙트를 위한 변수
+  const [gameEF, setGameEF] = useState("img/game_effect/Good.png");
+
+  const [loading, setLoading] = useState(null);
+  const [error, setError] = useState(null);
+
+  const videoRef = useRef(null);
+  const canvasRef = useRef(null);
+
   const getWebcam = (callback) => {
     try {
       const constraints = {
@@ -27,6 +43,7 @@ const SingleMode = () => {
       canvasRef.current.width = videoRef.current.videoWidth;
       canvasRef.current.height = videoRef.current.videoHeight;
 
+      
       if (ctx && ctx !== null) {
         if (videoRef.current) {
           ctx.translate(canvasRef.current.width, 0);
@@ -35,6 +52,15 @@ const SingleMode = () => {
           ctx.setTransform(1, 0, 0, 1, 0, 0);
           // sendImage();
         }
+        
+        const GEImage = new Image();
+        console.log(gameEF);
+        if (gameEF && gameEF !== null) {
+          GEImage.src = gameEF;
+  
+          ctx.drawImage(GEImage, 0,0);
+        }
+        
         ctx.fillStyle = "white";
         ctx.fillRect(10, 10, 100, 40);
         ctx.font = "15px Arial";
@@ -105,18 +131,7 @@ const SingleMode = () => {
 
 
 
-  const [websckt, setWebsckt] = useState();  
-  const [FPS, setFPS] = useState(5);  
-  const [Iv, setIv] = useState(null);  
-  const [playing, setPlaying] = useState(undefined);
-  const [timer, setTimer] = useState(undefined)
-  const [songID, setSongID] = useState(null);
 
-  const [loading, setLoading] = useState(null);
-  const [error, setError] = useState(null);
-
-  const videoRef = useRef(null);
-  const canvasRef = useRef(null);
 
   useEffect(() => {
 
@@ -137,7 +152,7 @@ const SingleMode = () => {
     const url = "ws://localhost:8000/client"
     const ws = new WebSocket(url);
     // var FPS = 5;
-    setFPS(5);
+    setFPS(0.5);
 
     ws.onopen = (event) => {
       console.log("ws.open");
@@ -145,15 +160,40 @@ const SingleMode = () => {
     // recieve message every start page
     ws.onmessage = (e) => {
       console.log(">>>>>>>>>>>>>>>>");
-      console.log(e.data);
+      console.log(e.data+" "+typeof(e.data));
+      let similarity = e.data;
+      
+      // const ctx = document.getElementById('canvas').getContext('2d');
+      // const img = new Image();
+      if (similarity < 0.15) {
+        setGameEF("img/game_effect/perfect.png");
+        console.log("perfect:"+gameEF)
+        // img.src = 'img/game_effect/perfect.png';
+      } else if (similarity < 0.22) {
+        setGameEF("img/game_effect/Good.png");
+        console.log("Good:"+gameEF)
+        // img.src = 'img/game_effect/Good.png';
+      }else {
+        setGameEF("img/game_effect/bad.png");
+        console.log("bad:"+gameEF)
+        // img.src = 'img/game_effect/bad.png';
+      }
+      // img.onload = function() {
+      //   ctx.drawImage(img, 30, 0);
+      //   // ctx.beginPath();
+      //   // ctx.moveTo(30, 96);
+      //   // ctx.lineTo(70, 66);
+      //   // ctx.lineTo(103, 76);
+      //   // ctx.lineTo(170, 15);
+      //   // ctx.stroke();
+      // };
     };
       
     ws.onclose = function(event) {
+      clearInterval(Iv);
       if (event.wasClean) {
-        clearInterval(Iv);
         alert(`[close] 커넥션이 정상적으로 종료되었습니다(code=${event.code} reason=${event.reason})`);
       } else {
-        clearInterval(Iv);
         // 예시: 프로세스가 죽거나 네트워크에 장애가 있는 경우
           // event.code가 1006이 됩니다.
         alert('[close] 커넥션이 죽었습니다.');
--- 모션인식/모션인식코드/수진/prac2_main.py
+++ 모션인식/모션인식코드/수진/prac2_main.py
@@ -1,3 +1,17 @@
+from fastapi.responses import StreamingResponse, FileResponse
+import warnings
+from sklearn.preprocessing import Normalizer
+import matplotlib.pyplot as plt
+from tf_pose.networks import get_graph_path, model_wh
+from tf_pose.estimator import TfPoseEstimator
+from tf_pose import common
+import pandas as pd
+import tensorflow as tf
+import numpy as np
+import logging
+import time
+import sys
+import cv2
 import base64
 from pathlib import Path
 from fastapi import FastAPI, Header, Request, Response, WebSocket
@@ -7,15 +21,13 @@ from requests import get
 app = FastAPI()
 
 
-import cv2
-
 # @app.websocket("/ws")
 # async def websocket_endpoint(websocket: WebSocket):
 #     print(f"client connected : {websocket.client}")
 #     await websocket.accept() # client의 websocket 접속 허용
 #     await websocket.send_text(f"Welcome client : {websocket.client}")
 #     camera = cv2.VideoCapture(0,cv2.CAP_DSHOW)
-    
+
 #     while True:
 #         success, image = camera.read() # 웹캠 읽어오는 코드 -> while 문안에 있어야 동영상으로 보임
 #         if not success:
@@ -38,37 +50,26 @@ import cv2
 # 모션인식코드
 ########################################################################################
 
-import sys
-import time
-import logging
-import numpy as np
-import cv2
-import tensorflow as tf
-import pandas as pd
 
-from tf_pose import common
-from tf_pose.estimator import TfPoseEstimator
-from tf_pose.networks import get_graph_path, model_wh
-import matplotlib.pyplot as plt
-from sklearn.preprocessing import Normalizer
-import warnings
 warnings.filterwarnings('ignore')
 
 ########################################################################################
 camera = 0
 resize = '432x368'     # resize images before they are processed
-resize_out_ratio = 4.0 # resize heatmaps before they are post-processed
-model='mobilenet_v2_large'
+resize_out_ratio = 4.0  # resize heatmaps before they are post-processed
+model = 'mobilenet_v2_large'
 show_process = False
 tensorrt = False       # for tensorrt process
 ########################################################################################
 w, h = model_wh(resize)
 if w > 0 and h > 0:
-    e = TfPoseEstimator(get_graph_path(model), target_size=(w, h), trt_bool=False)
+    e = TfPoseEstimator(get_graph_path(
+        model), target_size=(w, h), trt_bool=False)
 else:
-    e = TfPoseEstimator(get_graph_path(model), target_size=(432, 368), trt_bool=False)
+    e = TfPoseEstimator(get_graph_path(
+        model), target_size=(432, 368), trt_bool=False)
 print('********* Model Ready *************')
-######################################################################################## -> 서버 열리고 한번만 실행하면 됨.
+# -> 서버 열리고 한번만 실행하면 됨.
 
 
 def findCosineSimilarity_1(source_representation, test_representation):
@@ -88,77 +89,80 @@ async def websocket_endpoint(websocket: WebSocket):
     await websocket.accept()
     while True:
         # trainer_video = r'dance_video/dancer.mp4'
-        keyp_list=np.genfromtxt("dancer_keyp_list.csv",delimiter=",")
+        keyp_list = np.genfromtxt("dancer_keyp_list.csv", delimiter=",")
 
-        dim=(420,720)
+        dim = (420, 720)
         prev_time = 0
         # FPS = 1
         cnt = 0
         while True:
-            if cnt>=len(keyp_list): 
+            if cnt >= len(keyp_list):
                 await websocket.close()
                 break
 
             data = await websocket.receive_text()
             print(cnt)
-            cnt+=1
-            img = cv2.imdecode(np.fromstring(base64.b64decode(data.split(',')[1]), np.uint8), cv2.IMREAD_COLOR)
+            cnt += 1
+            img = cv2.imdecode(np.fromstring(base64.b64decode(
+                data.split(',')[1]), np.uint8), cv2.IMREAD_COLOR)
             image_1 = img
-            e_d=0
+            e_d = 0
             # current_time = time.time() - prev_time
             # if img is not None  :
-                # resizing the images
+            # resizing the images
             image_1 = cv2.flip(image_1, 1)
-            image_1 = cv2.resize(image_1, dim, interpolation = cv2.INTER_AREA)
-            humans_2 = e.inference(image_1, resize_to_default=(w > 0 and h > 0),upsample_size=4.0 )
-            #Dancer keypoints and normalization
-            transformer = Normalizer().fit(keyp_list)  
-            keyp_list=transformer.transform(keyp_list)
+            image_1 = cv2.resize(image_1, dim, interpolation=cv2.INTER_AREA)
+            humans_2 = e.inference(image_1, resize_to_default=(
+                w > 0 and h > 0), upsample_size=4.0)
+            # Dancer keypoints and normalization
+            transformer = Normalizer().fit(keyp_list)
+            keyp_list = transformer.transform(keyp_list)
             # Getting User keypoints, normalization and comparing also plotting the keypoints and lines to the image
-            image_1 = TfPoseEstimator.draw_humans(image_1, humans_2, imgcopy=False) # 선 표시하는 코드
+            image_1 = TfPoseEstimator.draw_humans(
+                image_1, humans_2, imgcopy=False)  # 선 표시하는 코드
             npimg = np.copy(image_1)
             image_h, image_w = npimg.shape[:2]
             centers = {}
             # keypoints_list=[]
-            min_=0
+            min_ = 0
             for human in humans_2:
-                        # draw point
-                    for i in range(common.CocoPart.Background.value):
-                                if i not in human.body_parts.keys():
-                                        continue
+                # draw point
+                for i in range(common.CocoPart.Background.value):
+                    if i not in human.body_parts.keys():
+                        continue
 
-                                body_part = human.body_parts[i]
-                                x_axis=int(body_part.x * image_w + 0.5)
-                                y_axis=int(body_part.y * image_h + 0.5)
-                                center=[x_axis,y_axis]
-                                centers[i] = center
-                    k=-2
-                    features=[0]*36
-                    for j in range(0,18):
-                        k=k+2
-                        try:
-                            if k>=36:
-                                break
-                            features[k]=centers[j][0]
-                            features[k+1]=centers[j][1]
-                        except:
-                            features[k]=0
-                            features[k+1]=0
-                    features=transformer.transform([features])
-                    min_=100 # Intializing a value to get minimum cosine similarity score from the dancer array list with the user
+                    body_part = human.body_parts[i]
+                    x_axis = int(body_part.x * image_w + 0.5)
+                    y_axis = int(body_part.y * image_h + 0.5)
+                    center = [x_axis, y_axis]
+                    centers[i] = center
+                k = -2
+                features = [0]*36
+                for j in range(0, 18):
+                    k = k+2
+                    try:
+                        if k >= 36:
+                            break
+                        features[k] = centers[j][0]
+                        features[k+1] = centers[j][1]
+                    except:
+                        features[k] = 0
+                        features[k+1] = 0
+                features = transformer.transform([features])
+                min_ = 100  # Intializing a value to get minimum cosine similarity score from the dancer array list with the user
 
-                    sim_score=findCosineSimilarity_1(keyp_list[cnt],features[0])
-                    #Getting the minimum Cosine Similarity Score
-                    if min_>sim_score:
-                        min_=sim_score
+                sim_score = findCosineSimilarity_1(keyp_list[cnt], features[0])
+                # Getting the minimum Cosine Similarity Score
+                if min_ > sim_score:
+                    min_ = sim_score
             # Displaying the minimum cosine score
             cv2.putText(image_1, str(min_), (10, 30),
-                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 255), 2)
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 255), 2)
             # If the disctance is below the threshold
-            if min_<0.15:
+            if min_ < 0.15:
                 cv2.putText(image_1, "PERFECT", (120, 700),
                             cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
-            elif min_<0.22:
+            elif min_ < 0.22:
                 cv2.putText(image_1, "GREAT", (120, 700),
                             cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
             else:
@@ -168,31 +172,26 @@ async def websocket_endpoint(websocket: WebSocket):
                         cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
 
             ret, buffer = cv2.imencode('.jpg', image_1)
-            await websocket.send_bytes(bytearray(buffer))  # client 에 메시지 전달
+            await websocket.send_text(str(min_))  # client 에 메시지 전달
             # frame = buffer.tobytes()
-        
+
             prev_time = time.time()
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
             # else:
             #     cv2.waitKey(1)
-        
+
         # cv2.waitKey(1)
 
-
-
-
-
-from fastapi.responses import StreamingResponse,FileResponse
 
 some_file_path = "dance_video/dancer.mp4"
 
 
 @app.get("/game/dancer")
 async def main():
-    # def iterfile():  # 
-    #     with open(some_file_path, mode="rb") as file_like:  # 
-    #         yield from file_like  # 
+    # def iterfile():  #
+    #     with open(some_file_path, mode="rb") as file_like:  #
+    #         yield from file_like  #
 
     # return StreamingResponse(iterfile(), media_type="video/mp4")
     return FileResponse(some_file_path)
