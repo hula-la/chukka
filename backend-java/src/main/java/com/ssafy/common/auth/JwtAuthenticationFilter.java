b12ab86b581f3fc1501223f18336f3cbb92da5be
최지원 <c.jiwon3000@gmail.com>
2022-08-08 11:20:36 +0900

    🐛 JWT Filter 오타 수정

--- backend-java/src/main/java/com/ssafy/common/auth/JwtAuthenticationFilter.java
+++ backend-java/src/main/java/com/ssafy/common/auth/JwtAuthenticationFilter.java
@@ -73,7 +73,7 @@ public class JwtAuthenticationFilter extends BasicAuthenticationFilter {
         // 요청 헤더에 Authorization 키값에 jwt 토큰이 포함된 경우에만, 토큰 검증 및 인증 처리 로직 실행.
         if (token != null) {
             // 비로그인
-            if (token.equals("1")) {
+            if (token.equals("Bearer 1")) {
                 return new UsernamePasswordAuthenticationToken("1", null, null);
             }
             // parse the token and validate it (decode)
