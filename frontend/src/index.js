af1f2296ae25831e34a4b0899381942daa789424
홍성목 <swff07183@gmail.com>
2022-08-11 23:12:29 +0900

    Delete service-worker

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,7 +5,6 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import registerServiceWorker from './registerServiceWorker';
 
 import store from './app/store';
 
@@ -17,7 +16,6 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root'),
 );
-registerServiceWorker();
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
