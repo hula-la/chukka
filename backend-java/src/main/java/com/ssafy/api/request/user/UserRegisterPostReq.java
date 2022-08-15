8277971cca46caf5aaf2a91a32c481fdca06fc2b
Choi Jiwon <c.jiwon3000@gmail.com>
2022-08-15 17:44:06 +0900

    ✨ 회원가입 Validation 임의 추가
    # <이모지> <제목> 의 형식으로 제목을 아래 공백줄에 작성
    # 제목은 50자 이내 / 변경사항이 "무엇"인지 명확히 작성 / 끝에 마침표 금지
    ✨ 로그인 기능 추가
    
    # 바로 아래 공백은 지우지 마세요 (제목과 본문의 분리를 위함)
    
    ################
    # 본문(구체적인 내용)을 아랫줄에 작성
    # 여러 줄의 메시지를 작성할 땐 "-"로 구분 (한 줄은 72자 이내)
    - 회원 정보 확인
    - 토큰 발급 및 저장
    ################
    # 꼬릿말(footer)을 아랫줄에 작성 (현재 커밋과 관련된 이슈 번호 추가 등)
    # Jira Smart commit 사용 시 Jira 이슈 또한 종료할 수 있음.
    # 예) Close #7
    
    ################
    # ✨ : 새로운 기능 추가
    # 🐛 : 버그 수정
    # 📚 : 문서 수정
    # 🚨 : 테스트 코드 추가
    # 🔨 : 코드 리팩토
    # 📝 : 코드 의미에 영향을 주지 않는 변경사항
    # 🔧 : 기타 변경사항, 프로`덕션 코드 변경사항 없음
    ################

--- backend-java/src/main/java/com/ssafy/api/request/user/UserRegisterPostReq.java
+++ backend-java/src/main/java/com/ssafy/api/request/user/UserRegisterPostReq.java
@@ -5,6 +5,7 @@ import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.*;
 import java.util.Date;
 
 /**
@@ -15,23 +16,40 @@ import java.util.Date;
 @ApiModel("UserRegisterPostRequest")
 public class UserRegisterPostReq {
 
+	@NotBlank(message="아이디는 필수값입니다.")
+	@Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z]).{4,16}", message = "아이디는 4~16자 영문 대소문자, 숫자를 사용하세요.")
 	@ApiModelProperty(name="유저 ID", example="your_id")
 	String userId;
+	@NotBlank(message="비밀번호는 필수값입니다.")
+	@Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\\\W)(?=\\\\S+$).{8,16}", message = "비밀번호는 8~16자 영문 대소문자, 숫자, 특수문자를 사용하세요.")
 	@ApiModelProperty(name="유저 Password", example="your_password")
 	String userPw;
+	@NotBlank(message="이름은 필수값입니다.")
+	@Size(min = 2, message = "이름은 두 글자 이상이어야 합니다.")
 	@ApiModelProperty(name="유저 Name", example="your_name")
 	String userName;
+	@NotBlank(message="핸드폰 번호는 필수값입니다.")
+	@Pattern(regexp = "(?=01[016789])(?=\\\\d{3,4})(?=\\\\d{4})", message = "올바른 형식이 아닙니다. (올바른 형식 : 010-0000-0000)")
 	@ApiModelProperty(name="유저 Phone", example="010-1234-5678")
 	String userPhone;
+	@NotBlank(message="이메일은 필수값입니다.")
+	@Email(message = "이메일 형식이 맞지 않습니다.")
 	@ApiModelProperty(name="유저 Email", example="abcd@ssafy.com")
 	String userEmail;
+	@Min(value = 0)
+	@Max(value = 1)
 	@ApiModelProperty(name="유저 Gender", example="1")
 	int userGender;
-	@ApiModelProperty(name="유저 Name", example="img/profile.png")
+
+	@ApiModelProperty(name="유저 프로필", example="img/profile.png")
 	String userProfile;
 
+	@NotNull
+	@Past(message = "올바른 생년월일이 아닙니다.")
 	@ApiModelProperty(name="유저 생년월일", example="2022-01-01")
 	Date userBirth;
+	@NotBlank(message="닉네임은 필수값입니다.")
+	@Size(min = 2, max = 16,message = "닉네임은 두 글자 이상 열여섯 글자 이하이어야 합니다.")
 	@ApiModelProperty(name="유저 Nickname", example="your_nickname")
 	String userNickname;
 
