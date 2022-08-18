9cc1b3c5a7f1201a158fbec83b730be119bd68ea
홍성목 <swff07183@gmail.com>
2022-08-18 22:44:46 +0900

    bug fix

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 // import store from '../app/store';
 
 const client = axios.create({
-  // baseURL: 'https://i7e202.p.ssafy.io/api',
-  baseURL: 'http://127.0.0.1:8080/api',
+  baseURL: 'https://i7e202.p.ssafy.io/api',
+  // baseURL: 'http://127.0.0.1:8080/api',
   headers: {
     'Content-Type': 'application/json',
   },
